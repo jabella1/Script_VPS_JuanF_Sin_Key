f8382f4f26328c1bb182c08e7674999412b609e6
Juan F. Abella <83263980+jabella1@users.noreply.github.com>
2022-09-03 19:21:11 -0500

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -230,6 +230,50 @@ msg -bar
 wget https://raw.githubusercontent.com/khaledagn/VPS-AGN_English_Official/master/LINKS-LIBRARIES/SPR.sh -O /usr/bin/SPR >/dev/null 2>&1
 chmod +x /usr/bin/SPR
 
+#LATAM ADMRufu 31-03-2022
+install_ADMRufu() {
+  clear && clear
+  msg -bar
+  echo -ne "\033[1;97m Digite su slogan: \033[1;32m" && read slogan
+  tput cuu1 && tput dl1
+  echo -e "$slogan"
+  msg -bar
+  clear && clear
+  mkdir /etc/ADMRufu >/dev/null 2>&1
+  cd /etc
+  wget https://raw.githubusercontent.com/NetVPS/Multi-Script/main/R9/ADMRufu.tar.xz >/dev/null 2>&1
+  tar -xf ADMRufu.tar.xz >/dev/null 2>&1
+  chmod +x ADMRufu.tar.xz >/dev/null 2>&1
+  rm -rf ADMRufu.tar.xz
+  cd
+  chmod -R 755 /etc/ADMRufu
+  ADMRufu="/etc/ADMRufu" && [[ ! -d ${ADMRufu} ]] && mkdir ${ADMRufu}
+  ADM_inst="${ADMRufu}/install" && [[ ! -d ${ADM_inst} ]] && mkdir ${ADM_inst}
+  SCPinstal="$HOME/install"
+  rm -rf /usr/bin/menu
+  rm -rf /usr/bin/adm
+  rm -rf /usr/bin/ADMRufu
+  echo "$slogan" >/etc/ADMRufu/tmp/message.txt
+  echo "${ADMRufu}/menu" >/usr/bin/menu && chmod +x /usr/bin/menu
+  echo "${ADMRufu}/menu" >/usr/bin/adm && chmod +x /usr/bin/adm
+  echo "${ADMRufu}/menu" >/usr/bin/ADMRufu && chmod +x /usr/bin/ADMRufu
+  [[ -z $(echo $PATH | grep "/usr/games") ]] && echo 'if [[ $(echo $PATH|grep "/usr/games") = "" ]]; then PATH=$PATH:/usr/games; fi' >>/etc/bash.bashrc
+  echo '[[ $UID = 0 ]] && screen -dmS up /etc/ADMRufu/chekup.sh' >>/etc/bash.bashrc
+  echo 'v=$(cat /etc/ADMRufu/vercion)' >>/etc/bash.bashrc
+  echo '[[ -e /etc/ADMRufu/new_vercion ]] && up=$(cat /etc/ADMRufu/new_vercion) || up=$v' >>/etc/bash.bashrc
+  echo -e "[[ \$(date '+%s' -d \$up) -gt \$(date '+%s' -d \$(cat /etc/ADMRufu/vercion)) ]] && v2=\"Nueva Vercion disponible: \$v >>> \$up\" || v2=\"Script Vercion: \$v\"" >>/etc/bash.bashrc
+  echo '[[ -e "/etc/ADMRufu/tmp/message.txt" ]] && mess1="$(less /etc/ADMRufu/tmp/message.txt)"' >>/etc/bash.bashrc
+  echo '[[ -z "$mess1" ]] && mess1="@Rufu99"' >>/etc/bash.bashrc
+  echo 'clear && echo -e "\n$(figlet -f big.flf "  ADMRufu")\n        RESELLER : $mess1 \n\n   Para iniciar ADMRufu escriba:  menu \n\n   $v2\n\n"|lolcat' >>/etc/bash.bashrc
+
+  update-locale LANG=en_US.UTF-8 LANGUAGE=en
+  clear && clear
+  msg -bar
+  echo -e "\e[1;92m             >> INSTALACION COMPLETADA <<" && msg bar2
+  echo -e "      COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
+  echo -e "                      \033[1;41m  menu  \033[0;37m" && msg -bar2
+}
+
 
 #VPS-AGN 8.6 OFFICIAL
 install_official() {
@@ -319,12 +363,16 @@ install_official() {
 /bin/cp /etc/skel/.bashrc ~/
 /bin/cp /etc/skel/.bashrc /etc/bash.bashrc
 echo -ne " \e[1;93m [\e[1;32m1\e[1;93m]\033[1;31m > \e[1;97m INSTALAR 8.5x OFFICIAL By. @JuanFCol \e[97m \n"
+echo -ne " \e[1;93m [\e[1;32m3\e[1;93m]\033[1;31m > \033[1;97m INSTALAR ADMRufu MOD \e[97m \n"
 msg -bar
 echo -ne "\033[1;97mINGRESE SOLO EL NUMERO ACORDE A SU OPCION:\e[32m "
 read opcao
 case $opcao in
 1)
   install_official
+  ;;
+2)
+  install_ADMRufu
   ;;
 esac
 exit
